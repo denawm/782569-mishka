29dcf90e764c4077ca69fe964ad49e3ae231d2cb
Denis <den_awm@mail.ru>
2018-11-10 17:01:52 +0600

    Доделал каталог

--- source/js/script.js
+++ source/js/script.js
@@ -2,6 +2,7 @@ var main_nav = document.querySelector(".main-nav");
 var main_toggle = document.querySelector(".main-nav__toggle");
 var overlay = document.querySelector(".modal-overlay");
 var order_btn = document.querySelector(".js_order");
+var basket_btn = document.querySelectorAll(".item-cart__basket-btn");
 var popup_basket = document.querySelector(".modal-basket");
 
 main_nav.classList.remove("main-nav--nojs");
@@ -15,6 +16,14 @@ main_toggle.addEventListener("click", function() {
     main_nav.classList.remove("main-nav--opened");
   }
 });
+
+for(var i = 0; i < basket_btn.length; i++){
+  basket_btn[i].addEventListener("click", function (evt) {
+  evt.preventDefault();
+  popup_basket.classList.add("modal-show");
+  overlay.classList.add("overlay-show");
+});
+}
 
 order_btn.addEventListener("click", function(evt){
   evt.preventDefault();
