46cc29cbc5fdade83490dc1fe17b6a83f4854cca
Denis <den_awm@mail.ru>
2018-11-10 15:11:07 +0600

    js и модалка

--- source/js/script.js
+++ source/js/script.js
@@ -1,5 +1,8 @@
 var main_nav = document.querySelector(".main-nav");
 var main_toggle = document.querySelector(".main-nav__toggle");
+var overlay = document.querySelector(".modal-overlay");
+var order_btn = document.querySelector(".js_order");
+var popup_basket = document.querySelector(".modal-basket");
 
 main_nav.classList.remove("main-nav--nojs");
 
@@ -11,4 +14,15 @@ main_toggle.addEventListener("click", function() {
     main_nav.classList.add("main-nav--closed");
     main_nav.classList.remove("main-nav--opened");
   }
+});
+
+order_btn.addEventListener("click", function(evt){
+  evt.preventDefault();
+  popup_basket.classList.add("modal-show");
+  overlay.classList.add("overlay-show");
+});
+
+overlay.addEventListener("click", function(){
+  overlay.classList.remove("overlay-show");
+  popup_basket.classList.remove("modal-show");
 });
